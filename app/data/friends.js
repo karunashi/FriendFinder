53fdc53f4a0813741323dfbe2d0c913e52b32215
Brandon Chang <Brandon Chang>
2017-04-28 23:21:33 -0700

    Added to post call in survey.html's script. Moving onto apiRoutes.js

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+var friends = [
+{
+	name: "Brandon",
+	pic: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAtVAAAAJDFiMTViZTRmLTgyNjItNGI0Ny04OGEyLTc3NWE5M2JlMDc3Mw.jpg",
+	charInfo: [5, 4, 3, 2, 1, 2, 3, 4, 5, 1]
+},
+{
+	name: "Sad Frog",
+	pic: "https://media.giphy.com/media/8gH2QEIKBa22Y/giphy.gif",
+	charInfo: [1, 2, 3, 4, 5, 4, 3, 2, 1, 2]
+}
+]
