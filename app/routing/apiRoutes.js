b5a60833163d93c6e0c4bbe1576aea261ae21e6f
Brandon Chang <Brandon Chang>
2017-04-29 01:45:28 -0700

    Got functionality to workgit add -A! apiRoutes.js complete, the entirely of the app should be working now.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,12 @@
 var friends = require("../data/friends");
+var totalDiff = 50;
+var diff = 0;
+var friendMeter = [];
+var match;
+
+Array.prototype.min = function() {
+	return Math.min.apply(null, this);
+}
 
 module.exports = function(app) {
 	app.get("/api/friends", function(req, res) {
@@ -6,7 +14,35 @@ module.exports = function(app) {
 	});
 	app.post("/api/friends", function(req, res) {
 		console.log("Test1: " + req.body.charInfo);
-		console.log("Test2: "+ res);
-		// Have this section for when survey.html is complete, and should be able to calculate a good match. 
+		console.log("Test2: "+friends)
+		
+		
+
+		// Make a for-loop that will go through however many entries are in friends array, and then have it compare its score 
+		// For calulcating absolute values: https://blog.udemy.com/javascript-absolute-value/
+		for (var i = 0; i < friends.length; i++) {
+			totalDiff = 50;
+			diff = 0;
+
+			for (var x = 0; x < friends[i].charInfo.length; x++) {
+				diff += Math.abs(parseInt(req.body.charInfo[x]) - parseInt(friends[i].charInfo[x]));
+				// console.log(diff);
+			}
+			// console.log(diff);
+			if (diff < totalDiff) {
+				friendMeter.push(diff);
+				// console.log(friendMeter);
+
+			}
+			console.log("Testing Ping")
+		}
+			console.log("End of Listing: "+friendMeter)
+
+			var test = friendMeter.indexOf(friendMeter.min())
+			console.log("Lowest is in the indexOf: "+test)
+			res.json(friends[test])
+			friendMeter = [];
+		
+		friends.push(req.body);
 	});
 }
