3ae0f3e8395bd6634a730942c105d82ee633011f
Brandon Chang <Brandon Chang>
2017-04-29 02:56:05 -0700

    Remade the entire friends.js for data

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,12 +2,37 @@ var friends = [
 {
 	name: "Brandon",
 	pic: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAtVAAAAJDFiMTViZTRmLTgyNjItNGI0Ny04OGEyLTc3NWE5M2JlMDc3Mw.jpg",
-	charInfo: [5, 4, 3, 2, 1, 2, 3, 4, 5, 1]
+	charInfo: [4, 1, 3, 1, 3, 3, 3, 3, 3, 3]
 },
 {
-	name: "Sad Frog",
-	pic: "https://media.giphy.com/media/8gH2QEIKBa22Y/giphy.gif",
-	charInfo: [1, 2, 3, 4, 5, 4, 3, 2, 1, 2]
+	name: "Spirit of War",
+	pic: "https://media.giphy.com/media/5NAFkimjc1xja/giphy.gif",
+	charInfo: [2, 4, 1, 4, 4, 4, 1, 3, 1, 2]
+},
+{
+	name: "Spirit of Death",
+	pic: "https://media.giphy.com/media/g1Xhj41YJrFni/giphy.gif",
+	charInfo: [3, 5, 4, 4, 3, 2, 4, 2, 5, 1]
+},
+{
+	name: "Spirit of Terror",
+	pic: "https://media.giphy.com/media/9YusABrwE8ARq/giphy.gif",
+	charInfo: [5, 5, 4, 5, 1, 1, 5, 1, 5, 1]
+},
+{
+	name: "Spirit of Justice",
+	pic: "https://media.giphy.com/media/ToEUwoZxvw2oo/giphy.gif",
+	charInfo: [4, 3, 1, 5, 1, 5, 1, 5, 1, 5]
+},
+{
+	name: "Spirit of Magic",
+	pic: "https://media.giphy.com/media/YtUj8qVjdtu7e/giphy.gif",
+	charInfo: [5, 5, 1, 2, 3, 2, 4, 3, 3, 4]
+},
+{
+	name: "Holy Crusader",
+	pic: "https://media.giphy.com/media/8KuEQtZnxSthK/giphy.gif",
+	charInfo: [2, 2, 2, 2, 2, 4, 1, 4, 4, 5]
 }
 ]
 
