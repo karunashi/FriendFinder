bec78eb47333491568f1e981cd12b99bf59ca22d
Brandon Chang <Brandon Chang>
2017-04-29 00:18:26 -0700

    Fixed some typos to fix the functionality while testing with Nodemon on real-time constantly (trial-and-error). Added lots of console.logs for testing purposes which will need to be removed. Forgot to mention that I also added modal-structure in my previous commit.

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,3 +10,5 @@ var friends = [
 	charInfo: [1, 2, 3, 4, 5, 4, 3, 2, 1, 2]
 }
 ]
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,6 +5,8 @@ module.exports = function(app) {
 		res.json(friends);
 	});
 	app.post("/api/friends", function(req, res) {
+		console.log("Test1: " + req.body.charInfo);
+		console.log("Test2: "+ res);
 		// Have this section for when survey.html is complete, and should be able to calculate a good match. 
-	}})
+	});
 }
